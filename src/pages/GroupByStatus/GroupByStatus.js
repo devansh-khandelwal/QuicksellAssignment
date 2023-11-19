9df76ebdecf1cf17e1a45b00c841bcffc6235170
devansh-khandelwal <devanshk2107@gmail.com>
2023-11-19 21:00:11 +0530

    made final changes to GroupByUser page UI

--- src/pages/GroupByStatus/GroupByStatus.js
+++ src/pages/GroupByStatus/GroupByStatus.js
@@ -1,60 +1,92 @@
 import React from "react";
 import "./GroupByStatus.css";
 import Card from "../../components/Card/Card";
+import { tickets, users } from "../../data";
 
-// Assuming you have the data stored in a variable named apiData
-import { tickets, users } from "../../data"; // Adjust the import path
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import RadioButtonUncheckedIcon from "@mui/icons-material/RadioButtonUnchecked";
+import BorderColorOutlinedIcon from "@mui/icons-material/BorderColorOutlined";
+import PendingOutlinedIcon from "@mui/icons-material/PendingOutlined";
+import CancelIcon from "@mui/icons-material/Cancel";
+import AddIcon from "@mui/icons-material/Add";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 
 function GroupByStatus({ selectedItem2 }) {
-  const statusColumns = {
-    Todo: "Todo",
-    Backlog: "Backlog",
-    Done: "Done",
-    "In progress": "In progress",
-    Cancelled: "Cancelled",
+  const ticketStatuses = [
+    { status: "Todo", icon: <RadioButtonUncheckedIcon color="action" /> },
+    { status: "Backlog", icon: <BorderColorOutlinedIcon color="action" /> },
+    { status: "Done", icon: <CheckCircleIcon color="primary" /> },
+    { status: "In progress", icon: <PendingOutlinedIcon color="action" /> },
+    { status: "Cancelled", icon: <CancelIcon color="action" /> },
+  ];
+
+  const countTicketsByStatus = (status) => {
+    return tickets.filter((ticket) => ticket.status === status).length;
   };
 
   const renderTicketsByStatus = (status) => {
     let filteredTickets = tickets.filter((ticket) => ticket.status === status);
 
-    // Sorting based on selectedItem2
-    if (selectedItem2 === "Priority") {
-      // Sort by priority in descending order
-      filteredTickets = filteredTickets.sort((a, b) => b.priority - a.priority);
-    } else if (selectedItem2 === "Title") {
-      // Sort by title in ascending order
-      filteredTickets = filteredTickets.sort((a, b) =>
-        a.title.localeCompare(b.title)
-      );
+    if (selectedItem2 === "Priority" || selectedItem2 === "Title") {
+      filteredTickets = filteredTickets.sort((a, b) => {
+        if (selectedItem2 === "Priority") {
+          return b.priority - a.priority;
+        } else {
+          return a.title.localeCompare(b.title);
+        }
+      });
     }
 
-    return filteredTickets.map((ticket) => {
-      const user = users.find((u) => u.id === ticket.userId);
+    return (
+      <>
+        <div className="status-column">
+          <div className="status-heading">
+            <div className="status-group-left">
+              <div className="status-icons">
+                <StatusIcon status={status} />
+              </div>
+              <div className="status-text">{status}</div>
+              <div className="status-number-of-tickets">
+                {countTicketsByStatus(status)}
+              </div>
+            </div>
+            <div className="status-group-right">
+              <AddIcon />
+              <MoreHorizIcon />
+            </div>
+          </div>
 
-      return (
-        <Card
-          key={ticket.id}
-          id={ticket.id}
-          title={ticket.title}
-          tag={ticket.tag}
-          userId={ticket.userId}
-          status={ticket.status}
-          priority={ticket.priority}
-          name={user ? user.name : ""}
-          availability={user ? user.available : false}
-        />
-      );
-    });
+          {filteredTickets.map((ticket) => {
+            const user = users.find((u) => u.id === ticket.userId);
+
+            return (
+              <Card
+                key={ticket.id}
+                id={ticket.id}
+                title={ticket.title}
+                tag={ticket.tag}
+                userId={ticket.userId}
+                priority={ticket.priority}
+                name={user ? user.name : ""}
+                availability={user ? user.available : false}
+              />
+            );
+          })}
+        </div>
+      </>
+    );
+  };
+
+  const StatusIcon = ({ status }) => {
+    const statusInfo = ticketStatuses.find((s) => s.status === status);
+    return statusInfo ? (
+      <div className="card-status-icon">{statusInfo.icon}</div>
+    ) : null;
   };
 
   return (
     <div className="GroupByStatus">
-      {Object.keys(statusColumns).map((status) => (
-        <div key={status} className="status-column">
-          <h4>{statusColumns[status]}</h4>
-          {renderTicketsByStatus(status)}
-        </div>
-      ))}
+      {ticketStatuses.map((s) => renderTicketsByStatus(s.status))}
     </div>
   );
 }
